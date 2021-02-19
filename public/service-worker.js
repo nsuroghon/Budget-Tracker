d5f5534f45575e1047460b0aefee626bb5d1726c
Niv Suroghon <nsuroghon@gmail.com>
2021-02-19 18:18:34 -0500

    service-workers install, active and fetch listeners

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,3 @@
-console.log("Hi from your service-worker.js file!");
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
@@ -6,29 +5,22 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/index.js',
+  '/db.js',
   '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
 ];
 
 // Add code to install and register your service worker.
-// install
 self.addEventListener("install", function (evt) {
-    // pre cache image data
     evt.waitUntil(
       caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/routes/transaction"))
       );
-      
-    // pre cache all static assets
     evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-  
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
     self.skipWaiting();
   });
-// ^^^ If done successfully, you should see your static cache in your Application tab.
   
 // Add code to activate the service worker and remove old data from the cache.
   self.addEventListener("activate", function(evt) {
@@ -50,7 +42,35 @@ self.addEventListener("install", function (evt) {
 
 // // Enable the service worker to intercept network requests.
   self.addEventListener('fetch', function(evt) {
-    // code to handle requests goes here
+    const {url} = evt.request;
+  if (url.includes("/all") || url.includes("/find")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+  } else {
+    // respond from static cache, request is not for /api/*
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  }
     });
 
 // // Serve static files from the cache. Proceed with a network request when the resource is not in the cache. 
