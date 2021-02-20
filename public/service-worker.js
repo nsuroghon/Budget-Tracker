fa0f4c6dee7c9fb400382bdb01df4a7980dd0625
Niv Suroghon <nsuroghon@gmail.com>
2021-02-19 22:32:35 -0500

    updating files to cache and install listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,22 +3,24 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   '/',
+  '/db.js',
   '/index.html',
   '/index.js',
-  '/db.js',
+  '/manifest.webmanifest',
   '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
 ];
 
 // Add code to install and register your service worker.
-self.addEventListener("install", function (evt) {
+  self.addEventListener("install", function(evt) {
     evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/routes/transaction"))
-      );
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+  
     self.skipWaiting();
   });
   
